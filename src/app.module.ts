6790969e248b7f9d11a4352e53b0ca69da749bcb
puntawach <puntawach.ch@gmail.com>
2026-03-23 15:47:47 +0700

    feat(columns): generate columns resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { WorkspaceModule } from './workspace/workspace.module';
+import { ColumnsModule } from './columns/columns.module';
 
 @Module({
-  imports: [AuthModule, UserModule, WorkspaceModule],
+  imports: [AuthModule, UserModule, WorkspaceModule, ColumnsModule],
   controllers: [AppController],
   providers: [AppService],
 })
